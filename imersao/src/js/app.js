1db6e94eaf59bc5219c041ec00256f0d0f2c6469
Paulo Horn <paulovictorhorn@gmail.com>
2024-09-05 22:11:38 -0300

    funcinalidade-do-botao-pesquisar

--- imersao/src/js/app.js
+++ imersao/src/js/app.js
@@ -1,2 +1,19 @@
-console.log(dados);
-alert("Olá, mundo!");
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+
+let resultados = "";
+
+for (let dado of dados) {
+    resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}.</p>
+                <a href="https://pt.wikipedia.org/wiki/Rayssa_Leal" target="_blank">${dado.link}</a>
+            </div>
+`
+};
+
+section.innerHTML = resultados;
+}
