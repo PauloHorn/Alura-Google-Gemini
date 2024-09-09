ea8302478bad665c2cc949e1c0cc4ea669a54cdd
Paulo Horn <paulovictorhorn@gmail.com>
2024-09-08 22:41:47 -0300

    interatividades-no-javascript

--- imersao/src/js/app.js
+++ imersao/src/js/app.js
@@ -1,19 +1,52 @@
 function pesquisar() {
-    let section = document.getElementById("resultados-pesquisa")
+    let section = document.getElementById("resultados-pesquisa");
 
-let resultados = "";
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tag = "";
 
-for (let dado of dados) {
-    resultados += `
-            <div class="item-resultado">
-                <h2>
-                    <a href="#" target="_blank">${dado.titulo}</a>
-                </h2>
-                <p class="descricao-meta">${dado.descricao}.</p>
-                <a href="https://pt.wikipedia.org/wiki/Rayssa_Leal" target="_blank">${dado.link}</a>
-            </div>
-`
-};
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    if (campoPesquisa == "" || campoPesquisa == " ") {
+        resultados = `
+                <div class="item-resultado">
+                    <p class="descricao-meta">Digite um nome.</p>
+                </div>
+                    `;
+        
+                    section.innerHTML = resultados;
+                    return
+    } else {
+        for (let dado of dados) {
+            titulo = dado.titulo.toLowerCase();
+            descricao = dado.descricao.toLowerCase();
+            tag = dado.tags.toLowerCase();
+
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tag.includes(campoPesquisa)) {
+            resultados += `
+                <div class="item-resultado">
+                    <h2>
+                        <a href="#" target="_blank">${dado.titulo}</a>
+                    </h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href="https://pt.wikipedia.org/wiki/Rayssa_Leal" target="_blank">${dado.link}</a>
+                </div>
+                        `
+        };
+    }
+    } 
+ 
+    if (!resultados) {
+        resultados = `
+                <div class="item-resultado">
+                    <p class="descricao-meta">Digite um nome ou termo válido.</p>
+                </div>
+                    `;
+        
+                    section.innerHTML = resultados;
+    }
 section.innerHTML = resultados;
 }
